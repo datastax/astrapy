0856a68ff2d75d5a7798724c78ab6c991de61a40
CRW <chris.wilhite@datastax.com>
2021-04-30 10:28:10 -0400

    update readme

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
