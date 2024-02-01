1c859cdf349ef8770269a10a3aa48ab40bcacb49
Eric Hare <ericrhare@gmail.com>
2024-02-01 11:29:33 -0700

    FIX: restore importlib version detection for pypi

--- astrapy/__init__.py
+++ astrapy/__init__.py
@@ -14,28 +14,35 @@
 
 import toml
 import os
+import importlib.metadata
 
 from typing import Any
 
 
 def get_version() -> Any:
-    # Get the path to the pyproject.toml file
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    pyproject_path = os.path.join(dir_path, "..", "pyproject.toml")
-
-    # Read the pyproject.toml file and get the version from the poetry section
     try:
-        with open(pyproject_path, encoding="utf-8") as pyproject:
-            # Load the pyproject.toml file as a dictionary
-            file_contents = pyproject.read()
-            pyproject_data = toml.loads(file_contents)
+        # Poetry will create a __version__ attribute in the package's __init__.py file
+        return importlib.metadata.version(__package__)
 
-            # Return the version from the poetry section
-            return pyproject_data["tool"]["poetry"]["version"]
+    # If the package is not installed, we can still get the version from the pyproject.toml file
+    except importlib.metadata.PackageNotFoundError:
+        # Get the path to the pyproject.toml file
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        pyproject_path = os.path.join(dir_path, "..", "pyproject.toml")
 
-    # If the pyproject.toml file does not exist or the version is not found, return unknown
-    except (FileNotFoundError, KeyError):
-        return "unknown"
+        # Read the pyproject.toml file and get the version from the poetry section
+        try:
+            with open(pyproject_path, encoding="utf-8") as pyproject:
+                # Load the pyproject.toml file as a dictionary
+                file_contents = pyproject.read()
+                pyproject_data = toml.loads(file_contents)
+
+                # Return the version from the poetry section
+                return pyproject_data["tool"]["poetry"]["version"]
+
+        # If the pyproject.toml file does not exist or the version is not found, return unknown
+        except (FileNotFoundError, KeyError):
+            return "unknown"
 
 
 __version__ = get_version()
