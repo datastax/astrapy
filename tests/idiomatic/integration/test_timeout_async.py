af64f762d3ea5444714e0959550f10394bbfe951
Stefano Lottini <stefano.lottini@datastax.com>
2024-03-19 00:23:06 +0100

    a little sleep before the error-testing count_documents

--- tests/idiomatic/integration/test_timeout_async.py
+++ tests/idiomatic/integration/test_timeout_async.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
 import pytest
 
 from astrapy import AsyncCollection, AsyncDatabase
@@ -28,6 +29,7 @@ class TestTimeoutAsync:
         async_empty_collection: AsyncCollection,
     ) -> None:
         await async_empty_collection.insert_many([{"a": 1}] * 100)
+        await asyncio.sleep(2)
         assert await async_empty_collection.count_documents({}, upper_bound=150) == 100
 
         with pytest.raises(DataAPITimeoutException) as exc:
