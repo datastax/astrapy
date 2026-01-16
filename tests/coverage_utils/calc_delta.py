42cb9a969d17ff563719285b0c50d3b2e5fb1eac
Stefano Lottini <236640031+sl-at-ibm@users.noreply.github.com>
2026-01-17 00:42:28 +0100

    compute delta, take 1 + temp it reduction

--- tests/coverage_utils/calc_delta.py
+++ tests/coverage_utils/calc_delta.py
@@ -16,11 +16,7 @@ from __future__ import annotations
 
 import argparse
 import json
-from operator import itemgetter
-from typing import Any
-
-TABLE_GUTTER = 3
-FMT_NUM_DECIMALS = 2
+from typing import Any, no_type_check
 
 
 def parse_args() -> argparse.Namespace:
@@ -65,18 +61,27 @@ def calc_delta(prev_j: dict[str, Any], new_j: dict[str, Any]) -> dict[str, Any]:
             old_coverage: float = pj["files"][f_name]["summary"]["percent_covered"]
             return new_coverage - old_coverage
 
-    per_file_deltas = dict(
+    per_file_deltas: dict[str, Any] = dict(
         sorted(
             [
-                (new_file, _file_delta(prev_j, new_j, f_name=new_file))
-                for new_file in new_j["files"].keys()
+                (
+                    new_file_k,
+                    {
+                        "delta": _file_delta(prev_j, new_j, f_name=new_file_k),
+                        **new_file_v["summary"],
+                    },
+                )
+                for new_file_k, new_file_v in new_j["files"].items()
             ],
-            key=itemgetter(1),
+            key=lambda file_pair: file_pair[1]["delta"],
         )
     )
     return {
-        "OVERALL": overall_delta,
-        **per_file_deltas,
+        "totals": {
+            "delta": overall_delta,
+            **new_j["totals"],
+        },
+        "files": per_file_deltas,
     }
 
 
@@ -84,20 +89,51 @@ def make_json_report(r_json: dict[str, float]) -> str:
     return json.dumps(r_json, indent=2)
 
 
+@no_type_check
 def make_table_report(r_json: dict[str, float]) -> str:
-    num_width = FMT_NUM_DECIMALS + 4
-    fmt_str = f"% {num_width}.{FMT_NUM_DECIMALS}f"
-    #
-    title_width = max(len(k) for k in r_json.keys()) + TABLE_GUTTER
-    row_width = title_width + TABLE_GUTTER + num_width
-    title_heading = (f"%-{title_width}s") % "File"
-    heading = f"{title_heading}{' ' * TABLE_GUTTER}{' ' * num_width}"
-    spacer = "-" * row_width
-    rows = [
-        f"%-{title_width}s{' ' * TABLE_GUTTER}{fmt_str}" % (delta_title, delta_percent)
-        for delta_title, delta_percent in r_json.items()
+    TABLE_GUTTER = 3
+    FMT_NUM_DECIMALS = 2
+    FMT_STR = f"% {FMT_NUM_DECIMALS + 4}.{FMT_NUM_DECIMALS}f%%"
+    COLUMNERS = [
+        ("File", lambda kp: kp[0]),
+        ("Stmts", lambda kp: f"{(kp[1]['num_statements'])}"),
+        ("Miss", lambda kp: f"{(kp[1]['missing_lines'])}"),
+        ("Cover", lambda kp: FMT_STR % kp[1]["percent_covered"]),
+        ("Delta", lambda kp: FMT_STR % kp[1]["delta"]),
     ]
-    return "\n".join([heading, spacer] + rows)
+
+    col_widths0 = [
+        max(len(col_n), len(col_v(("totals", r_json["totals"]))))
+        for col_n, col_v in COLUMNERS
+    ]
+    col_widths = [
+        max(
+            [len(col_v((file_k, file_v))) for file_k, file_v in r_json["files"].items()]
+            + [col_head_max]
+        )
+        for (_, col_v), col_head_max in zip(COLUMNERS, col_widths0)
+    ]
+
+    def _mk_line(file_k, file_v):
+        return (" " * TABLE_GUTTER).join(
+            [
+                (f"%{col_w}s") % col_v((file_k, file_v))
+                for (_, col_v), col_w in zip(COLUMNERS, col_widths)
+            ]
+        )
+
+    header_line = (" " * TABLE_GUTTER).join(
+        [(f"%{col_w}s") % col_k for (col_k, _), col_w in zip(COLUMNERS, col_widths)]
+    )
+    totals_line = _mk_line("totals", r_json["totals"])
+    line_width = sum(col_widths) + TABLE_GUTTER * (len(COLUMNERS) - 1)
+    sep_line = "-" * line_width
+
+    file_lines = [
+        _mk_line(file_k, file_v) for file_k, file_v in r_json["files"].items()
+    ]
+
+    return "\n".join([header_line, sep_line] + file_lines + [sep_line, totals_line])
 
 
 if __name__ == "__main__":
